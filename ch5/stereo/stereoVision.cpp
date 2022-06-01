2b2f5d079c994419d480fdaf07a10590885bd456
ljy-zju <junyl@zju.edu.cn>
2022-06-01 16:09:00 +0800

    ch3~ch7的代码阅读

--- ch5/stereo/stereoVision.cpp
+++ ch5/stereo/stereoVision.cpp
@@ -1,9 +1,9 @@
-#include <opencv2/opencv.hpp>
-#include <vector>
-#include <string>
 #include <Eigen/Core>
+#include <opencv2/opencv.hpp>
 #include <pangolin/pangolin.h>
+#include <string>
 #include <unistd.h>
+#include <vector>
 
 using namespace std;
 using namespace Eigen;
@@ -13,10 +13,10 @@ string left_file = "./left.png";
 string right_file = "./right.png";
 
 // 在pangolin中画图，已写好，无需调整
-void showPointCloud(
-    const vector<Vector4d, Eigen::aligned_allocator<Vector4d>> &pointcloud);
+void showPointCloud(const vector<Vector4d, Eigen::aligned_allocator<Vector4d>> &pointcloud);
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
 
     // 内参
     double fx = 718.856, fy = 718.856, cx = 607.1928, cy = 185.2157;
@@ -26,8 +26,8 @@ int main(int argc, char **argv) {
     // 读取图像
     cv::Mat left = cv::imread(left_file, 0);
     cv::Mat right = cv::imread(right_file, 0);
-    cv::Ptr<cv::StereoSGBM> sgbm = cv::StereoSGBM::create(
-        0, 96, 9, 8 * 9 * 9, 32 * 9 * 9, 1, 63, 10, 100, 32);    // 神奇的参数
+    cv::Ptr<cv::StereoSGBM> sgbm =
+        cv::StereoSGBM::create(0, 96, 9, 8 * 9 * 9, 32 * 9 * 9, 1, 63, 10, 100, 32); // 神奇的参数
     cv::Mat disparity_sgbm, disparity;
     sgbm->compute(left, right, disparity_sgbm);
     disparity_sgbm.convertTo(disparity, CV_32F, 1.0 / 16.0f);
@@ -37,8 +37,10 @@ int main(int argc, char **argv) {
 
     // 如果你的机器慢，请把后面的v++和u++改成v+=2, u+=2
     for (int v = 0; v < left.rows; v++)
-        for (int u = 0; u < left.cols; u++) {
-            if (disparity.at<float>(v, u) <= 0.0 || disparity.at<float>(v, u) >= 96.0) continue;
+        for (int u = 0; u < left.cols; u++)
+        {
+            if (disparity.at<float>(v, u) <= 0.0 || disparity.at<float>(v, u) >= 96.0)
+                continue;
 
             Vector4d point(0, 0, 0, left.at<uchar>(v, u) / 255.0); // 前三维为xyz,第四维为颜色
 
@@ -60,9 +62,11 @@ int main(int argc, char **argv) {
     return 0;
 }
 
-void showPointCloud(const vector<Vector4d, Eigen::aligned_allocator<Vector4d>> &pointcloud) {
+void showPointCloud(const vector<Vector4d, Eigen::aligned_allocator<Vector4d>> &pointcloud)
+{
 
-    if (pointcloud.empty()) {
+    if (pointcloud.empty())
+    {
         cerr << "Point cloud is empty!" << endl;
         return;
     }
@@ -72,16 +76,15 @@ void showPointCloud(const vector<Vector4d, Eigen::aligned_allocator<Vector4d>> &
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
-    pangolin::OpenGlRenderState s_cam(
-        pangolin::ProjectionMatrix(1024, 768, 500, 500, 512, 389, 0.1, 1000),
-        pangolin::ModelViewLookAt(0, -0.1, -1.8, 0, 0, 0, 0.0, -1.0, 0.0)
-    );
+    pangolin::OpenGlRenderState s_cam(pangolin::ProjectionMatrix(1024, 768, 500, 500, 512, 389, 0.1, 1000),
+                                      pangolin::ModelViewLookAt(0, -0.1, -1.8, 0, 0, 0, 0.0, -1.0, 0.0));
 
     pangolin::View &d_cam = pangolin::CreateDisplay()
-        .SetBounds(0.0, 1.0, pangolin::Attach::Pix(175), 1.0, -1024.0f / 768.0f)
-        .SetHandler(new pangolin::Handler3D(s_cam));
+                                .SetBounds(0.0, 1.0, pangolin::Attach::Pix(175), 1.0, -1024.0f / 768.0f)
+                                .SetHandler(new pangolin::Handler3D(s_cam));
 
-    while (pangolin::ShouldQuit() == false) {
+    while (pangolin::ShouldQuit() == false)
+    {
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         d_cam.Activate(s_cam);
@@ -89,13 +92,14 @@ void showPointCloud(const vector<Vector4d, Eigen::aligned_allocator<Vector4d>> &
 
         glPointSize(2);
         glBegin(GL_POINTS);
-        for (auto &p: pointcloud) {
+        for (auto &p : pointcloud)
+        {
             glColor3f(p[3], p[3], p[3]);
             glVertex3d(p[0], p[1], p[2]);
         }
         glEnd();
         pangolin::FinishFrame();
-        usleep(5000);   // sleep 5 ms
+        usleep(5000); // sleep 5 ms
     }
     return;
 }
